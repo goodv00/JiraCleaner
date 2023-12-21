4aaa216e671872a2cded5ed06563fea72d11673d
Victor Gutgesell <victor.gutgesell@unibas.ch>
2023-12-21 10:56:38 +0100

    Created the printout of issues to be deleted

--- jiraCleanup.py
+++ jiraCleanup.py
@@ -35,6 +35,8 @@ for issue in issues:  # Let's sort through the issue summaries and list versions
         except:
             # ATTENTION -> Buffer needs to be added as a list with the [] to form the first element
             issue_dict[summary_split[0]] = [buffer]
+del summary_split, issue
+
 '''
 returns a dictionary with the structure: 
 issue_dict =    {"SOFTWARE NAME": [["VERSION as [MAJOR, MINOR, DIST]",","JIRA ISSUE KEY"],["VERSION"...
@@ -43,11 +45,37 @@ The issue key is needed for the deletion request
 '''
 
 to_be_deleted = []  # List of issue keys to be deleted
+to_be_deleted_dict = {}
 
 for software_name in issue_dict:  # Takes each package to determine the issue keys to be deleted
     issue_dict[software_name].sort(reverse=True)  # Sorts the version numbers in descending order
     if len(issue_dict[software_name])>jira_config['VersionsToKeep']:
-        to_be_deleted.extend(issue_dict[software_name][jira_config['VersionsToKeep']:])
+        to_be_deleted_dict[software_name] = []
+        buffer = issue_dict[software_name][jira_config['VersionsToKeep']:]
+        to_be_deleted.extend(buffer)
+        buffer2 = ""
+        for i in buffer:
+            buffer2 = ".".join([str(i) for i in i[0]])
+            to_be_deleted_dict[software_name].append(buffer2)
+del buffer, buffer2, software_name, i
+
+ans1 = input("Would you like to see the versions to be deleted [y/n] ?")
+try:
+    if ans1.lower() == "y":
+
+        for software_name in to_be_deleted_dict:
+            printout = software_name + "\n"
+            for version in to_be_deleted_dict[software_name]:
+                printout += "\t\t" + version + "\n"
+            print(printout)
+
+        print("Done deleting")
+    elif ans1.lower() == "n":
+        print("Maybe next time...")
+    else:
+        print("No valid answer...")
+except:
+    print("No valid answer...")
 
 
 # This will pull the trigger (deletes all in to_be_deleted) from the Jira Board
